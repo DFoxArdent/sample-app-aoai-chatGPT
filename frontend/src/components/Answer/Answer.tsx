0a1eac5d46bd4672b428ecd976bfc6e3bd88ad21
DFoxArdent <DarylFox@ardent-management.com>
2024-04-09 10:20:15 +0100

    Add support for promptflow endpoint, remove rougue console log, citation styles in mobile, fix static files, show relevant error messages and don't call history/update when message content is empty

--- frontend/src/components/Answer/Answer.tsx
+++ frontend/src/components/Answer/Answer.tsx
@@ -12,6 +12,8 @@ import { parseAnswer } from "./AnswerParser";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import supersub from 'remark-supersub'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { nord } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { ThumbDislike20Filled, ThumbLike20Filled } from "@fluentui/react-icons";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
@@ -178,6 +180,22 @@ export const Answer = ({
         );
     }
 
+    const components = {
+        code({ node, ...props }: { node: any, [key: string]: any }) {
+            let language;
+            if (props.className) {
+                const match = props.className.match(/language-(\w+)/);
+                language = match ? match[1] : undefined;
+            }
+            const codeString = node.children[0].value ?? '';
+            return (
+                <SyntaxHighlighter style={nord} language={language} PreTag="div" {...props}>
+                    {codeString}
+                </SyntaxHighlighter>
+            );
+        },
+    };
+
     return (
         <>
             <Stack className={styles.answerContainer} tabIndex={0}>
@@ -190,6 +208,7 @@ export const Answer = ({
                                 remarkPlugins={[remarkGfm, supersub]}
                                 children={SANITIZE_ANSWER ? DOMPurify.sanitize(parsedAnswer.markdownFormatText, { ALLOWED_TAGS: XSSAllowTags }) : parsedAnswer.markdownFormatText}
                                 className={styles.answerText}
+                                components={components}
                             />
                         </Stack.Item>
                         <Stack.Item className={styles.answerHeader}>
@@ -244,7 +263,8 @@ export const Answer = ({
                     </Stack.Item>
                 </Stack>
                 {chevronIsExpanded &&
-                    <div style={{ marginTop: 8, display: "flex", flexFlow: "wrap column", maxHeight: "150px", gap: "4px" }}>
+                    <div className={styles.citationWrapper} >
+
                         {parsedAnswer.citations.map((citation, idx) => {
                             return (
                                 <span
